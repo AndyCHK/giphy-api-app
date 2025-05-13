01a164621d0e249a2e1fb02194ec9494b06f7f0f
Andrés Echavalete <ranchox@gmail.com>
2025-05-13 14:14:48 -0300

    fix:

--- app/Domain/Interfaces/GiphyServiceInterface.php
+++ app/Domain/Interfaces/GiphyServiceInterface.php
@@ -5,26 +5,17 @@ namespace App\Domain\Interfaces;
 interface GiphyServiceInterface
 {
     /**
-     *
-     * @param string $query Search query
-     * @param int $limit Number of results to return
-     * @param int $offset Results offset
+     * @param string $query 
+     * @param int $limit
+     * @param int $offset
      * @return array
      */
     public function search(string $query, int $limit = 25, int $offset = 0): array;
 
     /**
-     *
      * @param string $id GIF ID
      * @return array|null
      */
     public function getById(string $id): ?array;
 
-    /**
-     *
-     * @param int $limit Number of results to return
-     * @param int $offset Results offset
-     * @return array
-     */
-    public function getTrending(int $limit = 25, int $offset = 0): array;
 } 
